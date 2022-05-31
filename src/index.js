37a7ed20e7422b297de8d36909acef9e5420f937
Isaac Blanco <nuclearhands2001@gmail.com>
2022-05-30 22:24:43 -0500

    Fix 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './styles/index.scss'
 import axios from 'axios'
 
-axios.defaults.baseURL = "https://vot-e.herokuapp.com/";
+axios.defaults.baseURL = "https://vot-e-backend.herokuapp.com/";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
